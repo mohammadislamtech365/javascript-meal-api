9ed25f6f27e9e3f9bb05a7202189e42260019451
Mohammad Islam <saikat_mist@yahoo.com>
2022-12-04 09:17:22 -0800

    fetched api data and display dynamic meal

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,24 @@
 function loadData(){
     fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
     .then(response=>response.json())
-    .then(data=>displayMeals(data));
+    .then(data=>displayMeals(data.meals));
 }
 
 function displayMeals(meals){
-    console.log(meals);
+    for(meal of meals){
+        console.log(meal);
+        var mealsContainer = document.getElementById("meals-container");
+        var mealCard = document.createElement("div");
+        mealCard.classList.add("meal-item");
+        mealCard.innerHTML = `
+        <div class="img-container">
+            <img src="${meal.strMealThumb
+            }" alt="">
+        </div>
+        <p class="food-description">${meal.strMeal}</p>
+        <p>${meal.strInstructions.slice(0,50)}</p>
+        `;
+        mealsContainer.appendChild(mealCard);
+    }
 }
 loadData();
